513f614c65ec0a5e07b8be0d254d86b0c672ae38
panyue <iamy.id@gmail.com>
2012-08-21 22:56:31 -0700

    initial site

---
+++ admin/language/english/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer'] = 'E-Sun &copy; ' . date('Y') . ' All Rights Reserved.';
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.sunandsunshine.com/esun/');
+define('HTTP_IMAGE', 'http://www.sunandsunshine.com/esun/image/');
+define('HTTP_ADMIN', 'http://www.sunandsunshine.com/esun/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.sunandsunshine.com/esun/');
+define('HTTPS_IMAGE', 'http://www.sunandsunshine.com/esun/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home4/sunandsu/public_html/esun/catalog/');
+define('DIR_SYSTEM', '/home4/sunandsu/public_html/esun/system/');
+define('DIR_DATABASE', '/home4/sunandsu/public_html/esun/system/database/');
+define('DIR_LANGUAGE', '/home4/sunandsu/public_html/esun/catalog/language/');
+define('DIR_TEMPLATE', '/home4/sunandsu/public_html/esun/catalog/view/theme/');
+define('DIR_CONFIG', '/home4/sunandsu/public_html/esun/system/config/');
+define('DIR_IMAGE', '/home4/sunandsu/public_html/esun/image/');
+define('DIR_CACHE', '/home4/sunandsu/public_html/esun/system/cache/');
+define('DIR_DOWNLOAD', '/home4/sunandsu/public_html/esun/download/');
+define('DIR_LOGS', '/home4/sunandsu/public_html/esun/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'sunandsu_pnc1');
+define('DB_PASSWORD', 'nkd4VOAtoN');
+define('DB_DATABASE', 'sunandsu_pnc1');
+define('DB_PREFIX', 'oc_');
+?>
